365083d961ed21f91a7df470a691024f0bcac355
QBeLock <teo.x.12345@gmail.com>
2022-07-17 19:42:00 +0200

    SFX

--- World/Wall.gd
+++ World/Wall.gd
@@ -1,5 +1,10 @@
 extends Sprite
 
+export var Miss_sounds = [preload("res://Gun/Shoot 2.wav"), preload("res://Gun/Shoot 2.wav"), preload("res://Gun/Shoot 2.wav"), preload("res://Gun/Shoot 2.wav"), preload("res://Gun/Shoot 2.wav"), preload("res://Gun/Shoot 2.wav")]
+
 
 func Damage_received(Damage, Gun_type):
-	pass
+	get_child(0).pitch_scale = rand_range(0.8, 1.2)
+	get_child(0).volume_db = rand_range(0.5, 1)
+	get_child(0).stream = Miss_sounds[Gun_type - 1]
+	get_child(0).play()

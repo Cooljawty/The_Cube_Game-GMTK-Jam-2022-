c20fce8ef678f9e34a0534615004212165bdbd4c
Jacari Harper <jacarihr@gmail.com>
2022-07-16 22:14:47 -0700

    Redone combo system

--- Enemy.gd
+++ Enemy.gd
@@ -1,15 +1,9 @@
 extends KinematicBody2D
 
 var dir = Vector2.ZERO
-var helth = 24.0
+export var health = 24.0
 
-var Last_damage_type = 0
-var Last_damage = 0
-var Last_damage_type_2 = 0
-var Last_damage_2 = 0
-var Last_spliter = 0
-var Last_major_damage
-
+var Current_combo = [10, 10]
 
 func _process(delta):
 	look_at(get_node("/root/Global").Player.global_position)
@@ -18,9 +12,15 @@ func _process(delta):
 	move_and_collide(dir)
 
 func Damage_received(Damage, Gun_type):
-	helth -= Damage
+	health -= Damage
 	
-	print("Enemy health:", helth)
+	Current_combo.append(Gun_type)
+	Current_combo.pop_front();
+	print(Current_combo)
+	if Current_combo == get_node("/root/Global").Combo1:
+		print("Combo1 activated")
+		health -= 5
+	
 	"""
 	if Last_spliter == 0:
 		if Last_damage_type == Gun_type or (Last_damage_type == 0 and not Last_damage_type_2 != 0):
@@ -38,35 +38,8 @@ func Damage_received(Damage, Gun_type):
 			Last_damage = 0
 			Last_damage_data_save(Damage, Gun_type)
 	"""
-	if helth < 0:
+	if health < 0:
 		self.queue_free()
-
-func Last_damage_data_save(Damage, Gun_type):
-	Last_damage += Damage
-	Last_damage_type = Gun_type
-	Last_spliter = 0
-	if Last_damage > 5.5:
-		if get_node("/root/Global").Combo_komponent_1.has(Last_major_damage) and get_node("/root/Global").Combo_komponent_2.has(Gun_type):
-			Last_major_damage = 0
-			helth -= 12
-		else:
-			Last_major_damage = Last_damage_type
-		Last_damage_type = 0
-		Last_damage = 0
-
-func Last_damage_data_save_2(Damage, Gun_type):
-	Last_damage_2 += Damage
-	Last_damage_type_2 = Gun_type
-	Last_spliter = 1
-	if Last_damage > 5.5:
-		if get_node("/root/Global").Combo_komponent_1.has(Last_major_damage) and get_node("/root/Global").Combo_komponent_2.has(Gun_type):
-			Last_major_damage = 0
-			helth -= 12
-		else:
-			Last_major_damage = Last_damage_type_2
-		Last_damage_type_2 = 0
-		Last_damage_2 = 0
-
 
 func _on_Area2D_area_entered(area):
 	if area.get_parent().name == "Bullet":

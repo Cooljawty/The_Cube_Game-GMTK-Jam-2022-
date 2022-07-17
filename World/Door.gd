094bdf4970b83774000f3e37e1ebef4724887fd7
QBeLock <teo.x.12345@gmail.com>
2022-07-17 19:57:53 +0200

    some stuff

--- World/Door.gd
+++ World/Door.gd
@@ -5,6 +5,6 @@ export var Miss_sounds = [preload("res://Gun/Shoot 2.wav"), preload("res://Gun/S
 
 func Damage_received(Damage, Gun_type):
 	get_child(0).pitch_scale = rand_range(0.8, 1.2)
-	get_child(0).volume_db = rand_range(0.5, 1)
+	get_child(0).volume_db = rand_range(-1, -0.4)
 	get_child(0).stream = Miss_sounds[Gun_type - 1]
 	get_child(0).play()

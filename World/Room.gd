249fb8a89ba0305ab82c804901e644e5da87181b
QBeLock <teo.x.12345@gmail.com>
2022-07-17 17:14:41 +0200

    World gen done

--- World/Room.gd
+++ World/Room.gd
@@ -8,12 +8,12 @@ func _ready():
 	for n in int(rand_range(5, 7)):
 		if randi() % 2 == 1:
 			var Spawn = Enemy.instance()
-			get_parent().add_child(Spawn)
+			add_child(Spawn)
 			Spawn.position.x = rand_range(-25, 25)
 			Spawn.position.y = rand_range(-25, 25)
 		else:
 			var Spawn = Box.instance()
-			get_parent().add_child(Spawn)
+			add_child(Spawn)
 			Spawn.rotation_degrees = rand_range(-180, 180)
 			Spawn.position.x = rand_range(-25, 25)
 			Spawn.position.y = rand_range(-25, 25)

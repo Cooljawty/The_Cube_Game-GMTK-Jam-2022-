89ef8ae481cbed5ac62057c143bebb1e7f85cda6
QBeLock <teo.x.12345@gmail.com>
2022-07-20 19:55:57 +0200

    Some more prep for grid

---
+++ World/Room_2.gd
@@ -0,0 +1,22 @@
+extends TileMap
+
+var Enemy = preload("res://Enemy.tscn")
+var Box = preload("res://World/Box.tscn")
+onready var World_ = get_parent()
+
+func _ready():
+	randomize()
+	print(World_)
+	for n in int(rand_range(5, 7)):
+		if randi() % 3 == 1:
+			var Spawn = Enemy.instance()
+			add_child(Spawn)
+			Spawn.position.x = rand_range(64, 640)
+			Spawn.position.y = rand_range(64, 640)
+		else:
+			var Spawn = Box.instance()
+			add_child(Spawn)
+			Spawn.rotation_degrees = rand_range(-180, 180)
+			Spawn.position.x = rand_range(64, 640)
+			Spawn.position.y = rand_range(64, 640)
+		set_cell(randi() % 8 + 1,randi() % 8 + 1, 1)
